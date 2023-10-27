9d40ba426cbc50d2d909fa8d66f8a9eaca28d62f
jaybz <jaybz.e@gmail.com>
2023-10-27 15:27:17 +0800

    Minor text fixes

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
 const Slider = require('./slider');
 const { Client } = require('touchportal-api');
 const TPClient = new Client();
-const pluginId = 'TPSlider';
+const pluginId = 'TPGenericSlider';
 const sliders = {};
 const createdStates = [];
 
 TPClient.on("Info", (data) => {
-    console.log("TP-Slider Started");
+    console.log("TP-GenericSlider Started");
 });
 
 TPClient.on("ConnectorShortIdNotification", (data) => {
     let idBits = data.connectorId.split("|");
 
     if (!(
-        idBits[0].endsWith("tpslider_range_connector") && idBits.length === 4
+        idBits[0].endsWith("tpgenericslider_range_connector") && idBits.length === 4
     )) {
         console.log("Invalid Generic Slider ID (contains the | character, or too long)");
         return;
@@ -39,7 +39,7 @@ TPClient.on("ConnectorShortIdNotification", (data) => {
 
             if (!createdStates.includes(sliderId)) {
                 createdStates.push(sliderId);
-                TPClient.createState("tpslider_" + sliderId, sliderId, Number(minBits[1]), undefined);
+                TPClient.createState("tpgenericslider_" + sliderId, sliderId, Number(minBits[1]), undefined);
             }
 
             break;
@@ -47,30 +47,30 @@ TPClient.on("ConnectorShortIdNotification", (data) => {
 });
 
 TPClient.on("ConnectorChange", (data) => {
-    console.log("TP-Slider Connector Change");
+    console.log("TP-GenericSlider Connector Change");
     switch(data.connectorId) {
-        case "tpslider_range_connector":
+        case "tpgenericslider_range_connector":
             let params = {};
             data.data.forEach((item) => { params[item.id] = item.value; });
 
             if(sliders.hasOwnProperty(params.tpsliderId)) {
                 let stateValue =  sliders[params.tpsliderId].valueFromSlider(data.value);
-                TPClient.stateUpdate("tpslider_%s" + params.tpsliderId, stateValue);
+                TPClient.stateUpdate("tpgenericslider_%s" + params.tpsliderId, stateValue);
             }
             break;
     }
 });
 
 TPClient.on("Action", (data) => {
-    console.log("TP-Slider Action");
+    console.log("TP-GenericSlider Action");
 
     switch(data.actionId) {
-        case "tpslider_set_value":
+        case "tpgenericslider_set_value":
             let params = {};
             data.data.forEach((item) => { params[item.id] = item.value; });
             if(sliders.hasOwnProperty(params.tpsliderId)) {
                 let sliderValue =  sliders[params.tpsliderId].valueToSlider(params.tpsliderValue);
-                TPClient.stateUpdate("tpslider_%s" + params.tpsliderId, params.tpsliderValue);
+                TPClient.stateUpdate("tpgenericslider_%s" + params.tpsliderId, params.tpsliderValue);
                 TPClient.connectorUpdate(sliders[params.tpsliderId].shortId, sliderValue, undefined, true);
             }
             break;
@@ -78,8 +78,8 @@ TPClient.on("Action", (data) => {
 });
 
 TPClient.on("Close", (data) => {
-    console.log("TP-Slider Closing");
-    createdStates.forEach((item) => { TPClient.removeState("tpslider_%s" + item); });
+    console.log("TP-GenericSlider Closing");
+    createdStates.forEach((item) => { TPClient.removeState("tpgenericslider_%s" + item); });
     process.exit(0);
 });
 
